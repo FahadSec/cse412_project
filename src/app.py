4e93fc2a97d6dad52d5a960a22cd16bb1882d4fe
Fahad <iFahadDes@gmail.com>
2022-11-24 12:25:38 -0700

    Added a search function

--- src/app.py
+++ src/app.py
@@ -11,16 +11,35 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cse412_dev"
 db = SQLAlchemy(app)
 
+def search(subject, number, ext):
+    results = db.session.execute(f"""
+        SELECT Course.subject, Course.course_number, Course.number_ext, Course.title, Professor.name, Server.link
+        FROM Section
+        LEFT JOIN Discord_for on Discord_for.section_number = Section.section_number 
+        LEFT JOIN Server on Server.server_id = Discord_for.server_id
+        INNER JOIN Teaches on Teaches.section_number = Section.section_number
+        INNER JOIN Professor on Professor.professor_id = Teaches.professor_id
+        INNER JOIN Scheduled on Scheduled.section_number = Section.section_number 
+        INNER JOIN Course on (Course.subject = Scheduled.subject 
+            AND Course.course_number=Scheduled.course_number 
+            AND Course.number_ext=Scheduled.number_ext)
+        WHERE Course.subject = :subject AND Course.course_number = :number AND Course.number_ext = :ext;
+    """, {
+        "subject" : subject,
+        "number" : number,
+        "ext" : ext
+        })
+    return results
+
 @app.route("/")
 def hello_world():
-
-    # example query: print all discord server links to console
-    results = db.session.execute(db.select(models.Server)).scalars()
+    results = search("CSE", 412, "")
     for r in results:
-        print (r.link)
+        print(r)
 
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     db.init_app(app)
     app.run()
+
